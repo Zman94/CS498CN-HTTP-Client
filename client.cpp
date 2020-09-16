5fe4a0faa96b104919da3fcef04a950ccd777b9a
Zman94 <zgleason94@gmail.com>
2020-09-16 18:44:43 -0400

    debugging

--- client.cpp
+++ client.cpp
@@ -47,143 +47,141 @@ size_t split(const string &txt, vector<string> &strs, char ch)
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
 {
-	if (sa->sa_family == AF_INET) {
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	}
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
 
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
 int main(int argc, char *argv[])
 {
-	int sockfd, numbytes;  
-	char buf[MAXDATASIZE];
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	char s[INET6_ADDRSTRLEN];
-	ofstream myfile;
+    int sockfd, numbytes;  
+    char buf[MAXDATASIZE];
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+    std::ofstream myfile;
 
-	if (argc != 2) {
-	    fprintf(stderr,"usage: client hostname\n");
-	    exit(1);
-	}
+    if (argc != 2) {
+        fprintf(stderr,"usage: client hostname\n");
+        exit(1);
+    }
 
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
 
-	char protocol[100];
-	char ip[100];
-	char auth[100];
+    char protocol[100];
+    char ip[100];
+    char auth[100];
     char port[10] = "80";
-    char page[100];
-	/* Get the parts of the input url */
-	sscanf(argv[1], "%99[^:]://%99[^/]/%99[^\n]", protocol, auth, page);
-	sscanf(auth, "%99[^:]:%99[^\n]", ip, port);
-	// cout << auth << "\nprotocol: " << protocol << "\nip: " << ip << "\nport: " << port << "\npage: " << page << "\n\n";
+    char page[8192];
+    /* Get the parts of the input url */
+    sscanf(argv[1], "%99[^:]://%99[^/]/%8192[^\n]", protocol, auth, page);
+    sscanf(auth, "%99[^:]:%99[^\n]", ip, port);
 
-	/* Test if http protocol */
-	int is_prot_http = strcmp("http", protocol);
-	if(is_prot_http > 0) {
-		myfile.open("output");
-		myfile << "INVALIDPROTOCOL";
-		myfile.close();
-		return 1;
-	}
+    /* Test if http protocol */
+    int is_prot_http = strcmp("http", protocol);
+    if(is_prot_http > 0) {
+        myfile.open("output");
+        myfile.write("INVALIDPROTOCOL", 15);
+        myfile.close();
+        return 1;
+    }
 
-	if ((rv = getaddrinfo(ip, port, &hints, &servinfo)) != 0) {
-		myfile.open("output");
-		myfile << "NOCONNECTION";
-		myfile.close();
-		return 1;
-	}
+    if ((rv = getaddrinfo(ip, port, &hints, &servinfo)) != 0) {
+        myfile.open("output");
+        myfile.write("NOCONNECTION", 12);
+        myfile.close();
+        return 1;
+    }
 
-	// loop through all the results and connect to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
-			continue;
-		}
+    // loop through all the results and connect to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            continue;
+        }
 
-		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			close(sockfd);
-			continue;
-		}
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            continue;
+        }
 
-		break;
-	}
+        break;
+    }
 
-	if (p == NULL) {
-		myfile.open("output");
-		myfile << "NOCONNECTION";
-		myfile.close();
-		return 2;
-	}
-	string get_request = "GET " + string(page) + " HTTP/1.0\r\n\r\n";
-	const char* get_request_type = get_request.c_str();
-	cout << "request: " << get_request_type;
+    if (p == NULL) {
+        myfile.open("output");
+        myfile.write("NOCONNECTION", 12);
+        myfile.close();
+        return 2;
+    }
+    string get_request = "GET " + string(page) + " HTTP/1.0\r\n\r\n";
+    const char* get_request_type = get_request.c_str();
 
-	int sent_bytes = send(sockfd, get_request_type, strlen(get_request_type), 0);
-	int recv_length = 1;
-	char buffer[4096];
-	bool parseHeader = true;
-	while(recv_length > 0){
-		recv_length = recv(sockfd, buffer, 4096, 0);
-		if(recv_length == 0){
-			cout << "End of receive\n";
-			break;
-		}
-		cout << buffer << "\n\n";
+    int sent_bytes = send(sockfd, get_request_type, strlen(get_request_type), 0);
+    int recv_length = 1;
+    char buffer[4096];
+    bool parseHeader = true;
+    while(recv_length > 0){
+        recv_length = recv(sockfd, buffer, 4096, 0);
+        if(recv_length == 0){
+            break;
+        }
 
-		if(parseHeader){
-			parseHeader = false;
-			string buffer_str = string(buffer);
-			vector<string> response;
+        if(parseHeader){
+            parseHeader = false;
+            string buffer_str = string(buffer);
+            vector<string> response;
 
-			split(buffer_str, response, ' ');
-			int response_code;
-			sscanf(response[1].c_str(), "%d", &response_code);
-			if(response_code == 404){
-				myfile.open("output");
-				myfile << "FILENOTFOUND";
-				myfile.close();
-				return 2;
-			}
+            split(buffer_str, response, ' ');
+            int response_code;
+            sscanf(response[1].c_str(), "%d", &response_code);
+            if(response_code == 404){
+                myfile.open("output");
+                myfile.write("FILENOTFOUND", 12);
+                myfile.close();
+                return 2;
+            }
 
-			istringstream resp(buffer);
-			string header;
-			string::size_type index;
-			while (getline(resp, header) && header != "\r") {
+            istringstream resp(buffer);
+            string header;
+            string::size_type index;
+            while (getline(resp, header) && header != "\r") {
 
-				index = header.find(':', 0);
-				if(index != std::string::npos) {
-					if(response_code == 301 && header.compare("Location")){
-						cout << "SERVER MOVED. New location.\n";
-					}
-					std::cout << header.substr(0, index) << "\n" <<header.substr(index + 1) << "\n";
-				}
-			}
-			myfile.open("output");
-			while (getline(resp, header) && header != "\r") {
-				myfile << header;
-			}
-			myfile.close();
-		}
-		else{
-			istringstream resp(buffer);
-			string header;
-			string::size_type index;
-			myfile.open("output");
-			while (getline(resp, header) && header != "\r") {
-				myfile << header;
-			}
-			myfile.close();
-		}
-	}
-	freeaddrinfo(servinfo); // all done with this structure
-	close(sockfd);
+                index = header.find(':', 0);
+                if(index != std::string::npos) {
+                    if(response_code == 301 && header.compare("Location")){
+                    }
+                }
+            }
+            myfile.open("output");
+            while (getline(resp, header) && header != "\r") {
+                char* write_out = strcpy(new char[header.length() + 1], header.c_str());
+                myfile.write(write_out, header.length() + 1);
+                delete[] write_out;
+            }
+            myfile.close();
+        }
+        else{
+            istringstream resp(buffer);
+            string header;
+            string::size_type index;
+            myfile.open("output");
+            while (getline(resp, header) && header != "\r") {
+                char* write_out = strcpy(new char[header.length() + 1], header.c_str());
+                myfile.write(write_out, header.length() + 1);
+                delete[] write_out;
+            }
+            myfile.close();
+        }
+    }
+    freeaddrinfo(servinfo); // all done with this structure
+    close(sockfd);
 
-	return 0;
+    return 0;
 }
 
 // New location Location: https://illinois.edu/
