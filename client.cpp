0ac3b89876a76e6203f3a8b23d0f41ffa09920ab
Zach Gleason <zgleason94@gmail.com>
2020-09-17 12:25:25 -0400

    Z dev (#1)
    * Final push
    It's not the cleanest code, but it's for an assignment and it works.

--- client.cpp
+++ client.cpp
@@ -16,107 +16,189 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <map>
+#include <sstream>
 using namespace std;
 
-#define PORT "3490" // the port client will be connecting to 
+#define PORT "3490" // the port client will be connecting to
 
-#define MAXDATASIZE 100 // max number of bytes we can get at once 
+#define MAXDATASIZE 100 // max number of bytes we can get at once
+
+size_t split(const string &txt, vector<string> &strs, char ch)
+{
+    size_t pos = txt.find(ch);
+    size_t initialPos = 0;
+    strs.clear();
+
+    // Decompose statement
+    while (pos != std::string::npos)
+    {
+        strs.push_back(txt.substr(initialPos, pos - initialPos));
+        initialPos = pos + 1;
+        pos = txt.find(ch, initialPos);
+    }
+
+    // Add the last one
+    strs.push_back(txt.substr(initialPos, std::min(pos, txt.size()) - initialPos + 1));
+
+    return strs.size();
+}
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
 {
-	if (sa->sa_family == AF_INET) {
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	}
+    if (sa->sa_family == AF_INET)
+    {
+        return &(((struct sockaddr_in *)sa)->sin_addr);
+    }
 
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+    return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
 
 int main(int argc, char *argv[])
 {
-	int sockfd, numbytes;  
-	char buf[MAXDATASIZE];
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	char s[INET6_ADDRSTRLEN];
+    int sockfd, numbytes;
+    char buf[MAXDATASIZE];
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+    std::ofstream myfile;
 
-	if (argc != 2) {
-	    fprintf(stderr,"usage: client hostname\n");
-	    exit(1);
-	}
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: client hostname\n");
+        exit(1);
+    }
 
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
 
-	char protocol[100];
-	char ip[100];
-	char auth[100];
+    char protocol[100];
+    char ip[100];
+    char auth[100];
     char port[10] = "80";
-    char page[100];
-	/* Get the parts of the input url */
-	sscanf(argv[1], "%99[^:]://%99[^/]/%99[^\n]", protocol, auth, page);
-	sscanf(auth, "%99[^:]:%99[^\n]", ip, port);
-	cout << auth << "\nprotocol: " << protocol << "\nip: " << ip << "\nport: " << port << "\npage: " << page << "\n";
+    char page[8192];
+    /* Get the parts of the input url */
+    sscanf(argv[1], "%99[^:]://%99[^/]/%8192[^\n]", protocol, auth, page);
+    sscanf(auth, "%99[^:]:%99[^\n]", ip, port);
 
-	/* Test if http protocol */
-	int is_prot_http = strcmp("http", protocol);
-	if(is_prot_http > 0) {
-		ofstream myfile;
-		myfile.open("output");
-		myfile << "INVALIDPROTOCOL";
-		myfile.close();
-		return 1;
-	}
+    /* Test if http protocol */
+    int is_prot_http = strcmp("http", protocol);
+    if (is_prot_http > 0)
+    {
+        myfile.open("output");
+        myfile.write("INVALIDPROTOCOL", 15);
+        myfile.close();
+        return 1;
+    }
 
-	if ((rv = getaddrinfo(ip, port, &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
-	cout << "rv = " << rv << "\n";
+    if ((rv = getaddrinfo(ip, port, &hints, &servinfo)) != 0)
+    {
+        myfile.open("output");
+        myfile.write("NOCONNECTION", 12);
+        myfile.close();
+        return 1;
+    }
 
-	// loop through all the results and connect to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
-			perror("client: socket");
-			continue;
-		}
+    // loop through all the results and connect to the first we can
+    for (p = servinfo; p != NULL; p = p->ai_next)
+    {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                             p->ai_protocol)) == -1)
+        {
+            continue;
+        }
 
-		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			close(sockfd);
-			perror("client: connect");
-			continue;
-		}
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+        {
+            close(sockfd);
+            continue;
+        }
 
-		break;
-	}
+        break;
+    }
 
-	if (p == NULL) {
-		ofstream myfile;
-		myfile.open("output");
-		myfile << "NOCONNECTION";
-		myfile.close();
-		return 2;
-	}
+    if (p == NULL)
+    {
+        myfile.open("output");
+        myfile.write("NOCONNECTION", 12);
+        myfile.close();
+        return 2;
+    }
+    string get_request = "GET " + string(page) + " HTTP/1.0\r\n\r\n";
+    const char *get_request_type = get_request.c_str();
 
-	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-			s, sizeof s);
-	printf("client: connecting to %s\n", s);
+    int sent_bytes = send(sockfd, get_request_type, strlen(get_request_type), 0);
+    int recv_length = 1;
+    int length = 0;
+    char buffer[8192];
+    bool parseHeader = true;
+    myfile.open("output");
+    while (recv_length > 0)
+    {
+        recv_length = recv(sockfd, buffer, 8192, 0);
+        if (recv_length == 0)
+        {
+            break;
+        }
 
-	freeaddrinfo(servinfo); // all done with this structure
+        int firstLine = true;
+        if (parseHeader)
+        {
+            parseHeader = false;
+            string buffer_str = string(buffer);
+            vector<string> response;
 
-	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
-	    perror("recv");
-	    exit(1);
-	}
+            split(buffer_str, response, ' ');
+            int response_code;
+            sscanf(response[1].c_str(), "%d", &response_code);
 
-	buf[numbytes] = '\0';
+            if (response_code == 404)
+            {
+                myfile.write("FILENOTFOUND", 12);
+                myfile.close();
+                return 2;
+            }
+            else if (response_code == 301)
+            {
+            }
 
-	printf("client: received '%s'\n",buf);
+            const char *target = "\r\n\r\n";
+            char *eoheader = buffer;
+            eoheader = strstr(buffer, target);
+            eoheader = eoheader + 4; //Remove target
+            length = recv_length - (eoheader - buffer);
+            int file_name = string(page).length();
+            if (strcmp(page, eoheader + length - file_name) == 0)
+            {
+                myfile.write(eoheader, length - file_name);
+            }
+            else
+            {
+                myfile.write(eoheader, length);
+            }
+            memset(buffer, 0, sizeof(buffer));
+        }
+        else
+        {
+            length = recv_length;
+            int file_name = string(page).length();
+            if (strcmp(page, buffer + length - file_name) == 0)
+            {
+                myfile.write(buffer, length - file_name);
+            }
+            else
+            {
+                myfile.write(buffer, length);
+            }
+            memset(buffer, 0, sizeof(buffer));
+        }
+    }
+    myfile.close();
+    freeaddrinfo(servinfo); // all done with this structure
+    close(sockfd);
 
-	close(sockfd);
-
-	return 0;
+    return 0;
 }
-
